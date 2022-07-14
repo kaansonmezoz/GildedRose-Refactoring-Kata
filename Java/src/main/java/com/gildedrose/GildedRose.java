6b09a9ad758827ad53516936076ebd395946c3e0
kaan.sonmezoz <kaan.sonmezoz@trendyol.com>
2022-07-14 21:09:07 +0300

    Moved item name comparisons to Item class as methods

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -50,10 +50,10 @@ Gilded Rose Requirements Specification
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!items[i].isAgedBrie()
+                && !items[i].isConcert()) {
                 if (items[i].isQualityGreaterThanLowerLimit()) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!items[i].isSulfuras()) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
@@ -61,7 +61,7 @@ Gilded Rose Requirements Specification
                 if (items[i].isQualityLowerThanUpperLimit()) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].isConcert()) {
                         if (items[i].sellIn < 11) {
                             if (items[i].isQualityLowerThanUpperLimit()) {
                                 items[i].quality = items[i].quality + 1;
@@ -77,15 +77,15 @@ Gilded Rose Requirements Specification
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].isSulfuras()) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!items[i].isAgedBrie()) {
+                    if (!items[i].isConcert()) {
                         if (items[i].isQualityGreaterThanLowerLimit()) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!items[i].isSulfuras()) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -5,6 +5,10 @@ public class Item {
     private static final int QUALITY_UPPER_LIMIT = 50;
     private static final int QUALITY_LOWER_LIMIT = 0;
 
+    private static final String SULFURAS_NAME = "Sulfuras, Hand of Ragnaros";
+    private static final String CONCERT_NAME = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String AGED_BRIE_NAME = "Aged Brie";
+
     public String name;
 
     public int sellIn;
@@ -25,6 +29,18 @@ public class Item {
     //TODO: 14/07/2022 kaan.sonmezoz: find better name
     public boolean isQualityGreaterThanLowerLimit() {
         return this.quality > QUALITY_LOWER_LIMIT;
+    }
+
+    public boolean isSulfuras() {
+        return name.equals(SULFURAS_NAME);
+    }
+
+    public boolean isConcert() {
+        return name.equals(CONCERT_NAME);
+    }
+
+    public boolean isAgedBrie() {
+        return name.equals(AGED_BRIE_NAME);
     }
 
     @Override
