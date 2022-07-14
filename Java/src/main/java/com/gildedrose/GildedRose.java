6b5408e4e557c135653169de6715a3ceba089a99
kaan.sonmezoz <kaan.sonmezoz@trendyol.com>
2022-07-14 20:43:03 +0300

    Extracted literal upper lower limits to fields

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,8 @@
 package com.gildedrose;
 
 class GildedRose {
+    private static final int QUALITY_UPPER_LIMIT = 50;
+    private static final int QUALITY_LOWER_LIMIT = 0;
 
 /*
 
@@ -52,24 +54,24 @@ Gilded Rose Requirements Specification
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
+                if (items[i].quality > QUALITY_LOWER_LIMIT) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
+                if (items[i].quality < QUALITY_UPPER_LIMIT) {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                            if (items[i].quality < QUALITY_UPPER_LIMIT) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
 
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                            if (items[i].quality < QUALITY_UPPER_LIMIT) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
@@ -84,7 +86,7 @@ Gilded Rose Requirements Specification
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
+                        if (items[i].quality > QUALITY_LOWER_LIMIT) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
                             }
@@ -93,11 +95,12 @@ Gilded Rose Requirements Specification
                         items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
+                    if (items[i].quality < QUALITY_UPPER_LIMIT) {
                         items[i].quality = items[i].quality + 1;
                     }
                 }
             }
         }
     }
+
 }
