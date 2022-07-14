b7a4cbbbb1d316ef3d436fe73baeccab365b1109
kaan.sonmezoz <kaan.sonmezoz@trendyol.com>
2022-07-14 22:25:43 +0300

    Refactored concert increase strategy

--- Java/src/main/java/com/gildedrose/quality/ConcertIncreaseQualityStrategy.java
+++ Java/src/main/java/com/gildedrose/quality/ConcertIncreaseQualityStrategy.java
@@ -5,18 +5,12 @@ import com.gildedrose.Item;
 public class ConcertIncreaseQualityStrategy implements IncreaseQualityStrategy {
     @Override
     public void increase(Item item) {
-        decideIncreaseQualityStrategyForConcert(item).increase(item);
-    }
-
-    private IncreaseQualityStrategy decideIncreaseQualityStrategyForConcert(Item item) {
-        IncreaseQualityStrategy increaseQualityStrategy;
-        if(item.sellIn >= 11) {
-            increaseQualityStrategy = new NormalIncreaseQualityStrategy();
-        } else if(item.sellIn >= 6){
-            increaseQualityStrategy = new TwiceIncreaseQualityStrategy();
+        if (item.sellIn >= 11) {
+            item.quality = item.quality + 1;
+        } else if (item.sellIn >= 6) {
+            item.quality = item.quality + 2;
         } else {
-            increaseQualityStrategy = new TripleIncreaseQualityStrategy();
+            item.quality = item.quality + 3;
         }
-        return increaseQualityStrategy;
     }
 }
